42b21d6d5f91260d859edcea7b8f0122bf0dacd1
shlapa <animatzilla@gmail.com>
2024-05-05 01:59:53 +0300

    6

--- testingMock/mock_test.go
+++ testingMock/mock_test.go
@@ -1,1 +1,31 @@
 package testingMock
+
+import (
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+type mockRand struct{ mock.Mock }
+
+func newMockRund() *mockRand { return &mockRand{} }
+
+var mockr mockRand
+
+func TestFirstRandMock(t *testing.T) {
+
+	var myperson:= MyPersonalStruct{
+		person: Person{
+			name:        "John Doe",
+			age:         30,
+			id:          2,
+			randEffects: RandNumberGenerator.getRandEffects(),
+		},
+		f64Num:  35.5,
+		message: "Hello World",
+	}
+
+	mockr := newMockRund()
+	mockr.On("getRandEffects", 10)
+
+	//quo:=
+}
