4a31d5981f6c740b33fb462b529e7c1ce48c1509
shlapa <animatzilla@gmail.com>
2024-05-05 00:07:25 +0300

    2

--- main/main.go
+++ main/main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+//region Testify
 type Number interface {
 	int | float64
 }
@@ -26,6 +27,27 @@ func Multiplication[T Number](a T, b T) (result T) {
 	return result
 }
 
+//endregion
+
+//region Mock
+type Person struct {
+	name string
+	id   int
+}
+
+type MyPersonalStruct struct {
+	person  Person
+	f64Num  float64
+	message string
+}
+
+func CheckEvenUsers(personalStruct MyPersonalStruct) (check bool) {
+	if personalStruct.person.id/2 != 0 {
+
+	}
+}
+
+//endregion
 func main() {
 	fmt.Print(Sum(3, 4))
 }
